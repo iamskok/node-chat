c2558a3aa249ac2c8a9f54d43d44be8cf3a3494e
Vladimir Skok <skok@vova.io>
2018-12-18 20:55:28 +0400

    clean-up client code

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,58 +2,48 @@ const submitRoom = document.getElementById('submit-room').addEventListener('clic
 	event.preventDefault();
 	const form = document.getElementById('new-room');
 	const formData = new FormData(form);
-
-	console.log('formData', [...formData.entries()]);
 	const url = form.getAttribute('action');
-	
-	(async () => {
+	(async() => {
 		const rawResponse = await fetch(url, {
 			method: 'POST',
 			body: formData
 		});
-
 		const html = await rawResponse.text();
 		document.getElementById('rooms').innerHTML += html;
-		console.log('HTML = ', html);
 	})();
 });
 
-const submitUsername = document.getElementById('submit-username').addEventListener('click', (event) => {
-	event.preventDefault();
-	const form = document.getElementById('username-form');
-	const formData = new FormData(form);
-
-	console.log('username formData', [...formData.entries()]);
-	const url = form.getAttribute('action');
-
-	(async () => {
-		const rawResponse = await fetch(url, {
-			method: 'POST',
-			body: formData
-		});
-
-		console.log('rawResponse:', rawResponse);
-
-		if (rawResponse.status !== 200) {
-			if (rawResponse.statusText) {
-				alert(rawResponse.statusText);
-			} else {
-				alert('Server Error 😈');
+const submitUsername = document.getElementById('submit-username')
+	.addEventListener('click', (event) => {
+		event.preventDefault();
+		const form = document.getElementById('username-form');
+		const formData = new FormData(form);
+		const url = form.getAttribute('action');
+		(async() => {
+			const rawResponse = await fetch(url, {
+				method: 'POST',
+				body: formData
+			});
+			if (rawResponse.status !== 200) {
+				if (rawResponse.statusText) {
+					alert(rawResponse.statusText);
+				} else {
+					alert('Server Error');
+				}
 			}
-		}
-	})();
+		})();
 });
 
 if (document.getElementById('rooms')) {
-	document.getElementById('rooms').addEventListener('click', function(evt) {
+	document.getElementById('rooms').addEventListener('click', function(e) {
 		let password;
-		evt.preventDefault();
-		const private = evt.target.parentNode.getAttribute('data-private') === 'true';
-		const id = evt.target.parentNode.getAttribute('data-id');
+		e.preventDefault();
+		const private = e.target.parentNode.getAttribute('data-private') === 'true';
+		const id = e.target.parentNode.getAttribute('data-id');
 		if (private) {
 			password = prompt('Enter room password');
 		}
-		document.querySelector('#room').setAttribute('action', evt.target.getAttribute('href'));
+		document.querySelector('#room').setAttribute('action', e.target.getAttribute('href'));
 		document.querySelector('#room input[name="id"]').value = id;
 		document.querySelector('#room input[name="password"]').value = password;
 		document.querySelector('#room input[type="submit"]').click();
@@ -67,24 +57,13 @@ window.addEventListener('click', (e) => {
 		document.execCommand('copy');
 	}
 });
-// document.getElementById('get-secret').blur();
-// const userSecret = document.getElementById('user-secret');
-// userSecret.focus();
-// // document.designMode = 'on';
-// userSecret.value = 'sfewfwefewf';
-// userSecret.select();
-// document.execCommand('copy');
 
 let secret = '';
-
 const getSecret = document.getElementById('get-secret')
 	.addEventListener('click', (event) => {
-		console.log('get-secret');
-
 		const xhr = new XMLHttpRequest();
 		xhr.open('GET', '/get-secret', true);
 		xhr.send();
-
 		function onLoad(e) {
 			if (xhr.status == 200) {
 				const userSecret = document.getElementById('user-secret');
--- static/scripts/room.js
+++ static/scripts/room.js
@@ -1,24 +1,19 @@
-const submitMessage = document.getElementById('submit-message').addEventListener('click', (event) => {
-	event.preventDefault();
+const submitMessage = document.getElementById('submit-message')
+	.addEventListener('click', (e) => {
+	e.preventDefault();
 	const form = document.getElementById('message-form');
 	const formData = new FormData(form);
-
-	//console.log('message formData', [...formData.entries()]);
 	const url = form.getAttribute('action');
-
-	(async () => {
+	(async() => {
 		const rawResponse = await fetch(url, {
 			method: 'POST',
 			body: formData
 		});
-
-		//console.log('rawResponse:', rawResponse);
-
 		if (rawResponse.status !== 200) {
 			if (rawResponse.statusText) {
 				alert(rawResponse.statusText);
 			} else {
-				alert('Server Error 😈');
+				alert('Server Error');
 			}
 		}
 	})();
@@ -31,12 +26,10 @@ const submitMessage = document.getElementById('submit-message').addEventListener
 		messageDates = Array.from(messages)
 			.map(item => parseInt(item.dataset.date))
 			.sort((a, b) => a > b);
-		//let lastDate = messages[messages.length - 1] || 0;
 		let lastDate = 0;
 		if (messageDates.length) {
 			lastDate = messageDates[messages.length - 1];
 		}
-		console.log('>>>>>>>>>>>>>>', lastDate);
 		const request = new XMLHttpRequest();
 		request.addEventListener('load', function() {
 			let data;
@@ -45,7 +38,6 @@ const submitMessage = document.getElementById('submit-message').addEventListener
 			} catch(e) {
 				data = [];
 			}
-			console.log('Get message from the server via polling:', data);
 			messages = messages.concat(data);
 			data.forEach(msg => {
 				const div = document.createElement('div');
@@ -54,46 +46,8 @@ const submitMessage = document.getElementById('submit-message').addEventListener
 			});
 			polling();
 		});
-
 		request.open("GET", `http://localhost:5000/polling?id=${id}&last=${lastDate}`);
 		request.send();
 	}
 	polling();
 })();
-
-// (function() {
-// 	let messages = [];
-
-// 	function polling() {
-// 		const request = new XMLHttpRequest();
-// 		request.addEventListener("load", function() {
-
-// 			const data = JSON.parse(this.responseText);
-// 			messages = messages.concat(data);
-// 			data.forEach(msg => {
-// 				const username = msg.username;
-// 				const message = msg.message;
-
-// 				let html = document.getElementById('chat-history').innerHTML;
-// 				const div = document.createElement('div');
-// 				div.innerHTML = `${username}: ${message}`;
-// 				document.getElementById('chat-history').appendChild(div);
-// 			});
-
-// 			polling();
-// 		});
-
-// 		let id;
-
-// 		if (!messages.length) {
-// 			id = 0;
-// 		} else {
-// 			id = messages[messages.length - 1].id;
-// 		}
-
-// 		request.open("GET", `http://localhost:5000/chat?id=${id}`);
-// 		request.send();
-// 	}
-
-// 	polling();
-// })();
